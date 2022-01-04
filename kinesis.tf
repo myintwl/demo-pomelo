4b665260eb9cc21b10927c17862e656887c71bcb
MYINT <luvmyo@gmail.com>
2022-01-04 23:28:43 +0700

    kinesis updated

--- kinesis.tf
+++ kinesis.tf
@@ -26,52 +26,34 @@ resource "aws_glue_catalog_table" "aws_glue_table" {
     location      = var.s3_bucket_path
     input_format  = var.storage_input_format
     output_format = var.storage_output_format
-    /*     columns = [
-      {
-        name = "key"
-        type = "string"
-      },
-      {
-        name = "value"
-        type = "string"
-      },
-    ] */
   }
 }
 
-resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
-  name        = "${var.name}_firehose_delivery_stream"
+resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
+  name        = "terraform-kinesis-firehose-extended-s3-test-stream"
   destination = "extended_s3"
 
-  kinesis_source_configuration {
-    kinesis_stream_arn = aws_kinesis_stream.kinesis_stream.arn
-    role_arn           = aws_iam_role.firehose_role.arn
-  }
-
-  //refer the more s3 configuration at https://docs.aws.amazon.com/firehose/latest/APIReference/API_ExtendedS3DestinationConfiguration.html
   extended_s3_configuration {
-    role_arn        = aws_iam_role.firehose_role.arn
-    bucket_arn      = var.s3_bucket_arn
-    buffer_size     = 100
-    buffer_interval = "300"
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.bucket.arn
 
-    kms_key_arn = data.aws_kms_alias.kms_encryption.arn
+    processing_configuration {
+      enabled = "true"
 
-    data_format_conversion_configuration {
-      input_format_configuration {
-        deserializer {
-          hive_json_ser_de {}
+      processors {
+        type = "Lambda"
+
+        parameters {
+          parameter_name  = "LambdaArn"
+          parameter_value = "${aws_lambda_function.lambda_sqs.arn}:$LATEST"
         }
       }
-
-      output_format_configuration {}
     }
-
-    /*  schema_configuration {
-      database_name = aws_glue_catalog_table.aws_glue_table.database_name
-      role_arn      = aws_iam_role.firehose_role.arn
-      table_name    = aws_glue_catalog_table.aws_glue_table.name
-      region        = var.region
-    } */
   }
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "tf-test-bucket"
+  acl    = "private"
+}
+
